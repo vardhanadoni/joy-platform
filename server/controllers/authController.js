47af56b65f9c4aed9f4222360a64f56717a24d29
vardhanadoni <vardhanadoni@gmail.com>
2025-06-01 19:41:43 +0530

    feat:jwt token expiration time change

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -6,7 +6,7 @@ const jwt = require('jsonwebtoken');
 
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: '1d', // Token expires in 1 hour
+    expiresIn: '2h', // Token expires in 1 day
   });
 };
 /**
